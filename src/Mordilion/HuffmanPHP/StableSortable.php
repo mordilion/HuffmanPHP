bf3e740217e8aab980b9cfbcb3906c803a2dfd96
Henning Huncke <mordilion@gmx.de>
2022-02-18 08:11:38 +0100

    use nativ functons php >= 8.0 and improve circleci

--- src/Mordilion/HuffmanPHP/StableSortable.php
+++ src/Mordilion/HuffmanPHP/StableSortable.php
@@ -6,37 +6,55 @@ namespace Mordilion\HuffmanPHP;
 
 trait StableSortable
 {
-    public function uasort(array &$array, callable $callback)
+    public function uasort(array &$array, callable $callback): bool
     {
+        if (version_compare(phpversion(), '8.0.0', '>=')) {
+            return uasort($array, $callback);
+        }
+
         $arrayAndPosition = $this->getArrayWithPosition($array);
 
-        usort($arrayAndPosition, function($a, $b) use($callback) {
+        $result = usort($arrayAndPosition, function($a, $b) use($callback) {
             return $callback($a['value'], $b['value']) ?: $a['position'] <=> $b['position'];
         });
 
         $array = array_combine(array_column($arrayAndPosition, 'key'), array_column($arrayAndPosition, 'value'));
+
+        return $result;
     }
 
-    public function uksort(array &$array, callable $callback)
+    public function uksort(array &$array, callable $callback): bool
     {
+        if (version_compare(phpversion(), '8.0.0', '>=')) {
+            return uksort($array, $callback);
+        }
+
         $arrayAndPosition = $this->getArrayWithPosition($array);
 
-        usort($arrayAndPosition, function($a, $b) use($callback) {
+        $result = usort($arrayAndPosition, function($a, $b) use($callback) {
             return $callback($a['key'], $b['key']) ?: $a['position'] <=> $b['position'];
         });
 
         $array = array_combine(array_column($arrayAndPosition, 'key'), array_column($arrayAndPosition, 'value'));
+    
+        return $result;
     }
 
-    public function usort(array &$array, callable $callback)
+    public function usort(array &$array, callable $callback): bool
     {
+        if (version_compare(phpversion(), '8.0.0', '>=')) {
+            return usort($array, $callback);
+        }
+
         $arrayAndPosition = $this->getArrayWithPosition($array);
 
-        usort($arrayAndPosition, function($a, $b) use($callback) {
+        $result = usort($arrayAndPosition, function($a, $b) use($callback) {
             return $callback($a['value'], $b['value']) ?: $a['position'] <=> $b['position'];
         });
 
         $array = array_column($arrayAndPosition, 'value');
+
+        return $result;
     }
 
     private function getArrayWithPosition(array $array): array
